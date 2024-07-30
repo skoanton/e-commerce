ee7f9c2a48dbf71cbfcc74f8a3313b3f83b0981c
skoanton <antonhakanssonn@gmail.com>
2024-07-30 15:55:41 +0200

    first header layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+import { Input } from "@/components/ui/input";
+import { Heart, Menu, Search, ShoppingBasket } from "lucide-react";
+import { Button } from "./ui/button";
+
+type HeaderProps = {};
+
+export default function Header({}: HeaderProps) {
+  return (
+    <>
+      <nav className="flex flex-col justify-between gap-5 p-2">
+        <section className="flex justify-between">
+          <h1 className="text-2xl font-bold uppercase text-orange-500">
+            Not a shop
+          </h1>
+          <ul>
+            <li>
+              <div>
+                <ul className="flex gap-5 items-center">
+                  <li>
+                    <Button>Sign in</Button>
+                  </li>
+                  <li>
+                    <Heart className="w-8 h-8" />
+                  </li>
+                  <li>
+                    <ShoppingBasket className="w-8 h-8" />
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <ul>
+            <li className="flex gap-3 items-center">
+              <Menu className="w-8 h-8" />
+
+              <div className=" w-full relative">
+                <Input placeholder="Search for a product" />
+                <div className="absolute right-2 bottom-2">
+                  <Search />
+                </div>
+              </div>
+            </li>
+          </ul>
+        </section>
+      </nav>
+    </>
+  );
+}
