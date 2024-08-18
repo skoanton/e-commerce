da6d85b21ae835ebf61553497937fb5d143cf2c4
skoanton <antonhakanssonn@gmail.com>
2024-08-18 12:17:20 +0200

    header done, but problems with big to small with icons not moving correctly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,11 +7,11 @@ type HeaderProps = {};
 export default function Header({}: HeaderProps) {
   return (
     <>
-      <nav className="flex flex-col justify-between gap-5 p-2">
-        <section className="flex justify-between">
-          <h1 className="text-2xl font-bold uppercase text-orange-500">
-            Not a shop
-          </h1>
+      <nav className="flex flex-col justify-between gap-5 p-2 md:flex-row">
+        <h1 className="text-2xl font-bold uppercase text-orange-500 md:order-1">
+          Not a shop
+        </h1>
+        <section className="md:order-3">
           <ul>
             <li>
               <div>
@@ -30,7 +30,7 @@ export default function Header({}: HeaderProps) {
             </li>
           </ul>
         </section>
-        <section>
+        <section className="md:order-2 md:flex-grow">
           <ul>
             <li className="flex gap-3 items-center">
               <Menu className="w-8 h-8" />
